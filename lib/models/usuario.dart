4179f676dafbf941a136834ae2c9590b0deb5061
tonitahoces <tonitahoces@gmail.com>
2021-01-31 18:40:07 +0100

    mejoras

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Usuario {
   String nombre;
   int edad;
-  List<String> profesiones;
+  List<String> profesiones = [];
 
   Usuario({@required this.nombre, this.edad, this.profesiones})
       : assert(nombre != null);
